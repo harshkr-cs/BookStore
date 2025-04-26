52641df7b6ccaaca5dbf9d0b3f86ee961e3fcc79
harshkr-cs <harshwwe555@gmail.com>
2025-04-26 12:13:38 +0530

    test

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.use('/api/stats', statsRoutes);
 
 //Change
 app.get('/api', (req, res) => {
-    res.send('My Bookstore backend is liver!');
+    res.send('My Bookstore backend is live!');
 });
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
